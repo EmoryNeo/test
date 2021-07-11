a094454a5d46136a4e7f0fd0602b1452e8f2fc4c
EmoryNeo <iceorangeduxiaocheng@aliyun.com>
2021-07-11 19:08:58 +0800

    测试第一次提交到本地库

---
+++ test/src/main/java/edu/cczu/test/TestMain.java
@@ -0,0 +1,13 @@
+package edu.cczu.test;
+
+/**
+ * @author neo@gmail.com
+ * @date 2021/7/11 19:03
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git Version Control...");
+    }
+
+}
